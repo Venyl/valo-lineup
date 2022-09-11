52bd3a29faa806e3639ab8116f3eeb771ab252f9
Venyl <zexe2121@gmail.com>
2022-09-11 17:38:45 +0200

    Remove useless Dropdown component

--- src/components/Dropdown.tsx
+++
@@ -1,9 +0,0 @@
-import DropdownItem from './DropdownItem';
-
-interface props {
-    name: string;
-}
-
-export default function Dropdown({ name }: props) {
-    return <DropdownItem>{name}</DropdownItem>;
-}
