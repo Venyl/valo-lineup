020c9b68227385464770a24d15890f7a90c07397
Venyl <zexe2121@gmail.com>
2022-09-11 18:01:04 +0200

    Change agent icon src

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -7,11 +7,17 @@ interface props {
 
 export function Agent({ agentName }: props) {
     const { setAgent } = useContext(AgentContext);
+    const agentImgs = {
+        'KAY/O': 'https://static.wikia.nocookie.net/valorant/images/f/f0/KAYO_icon.png',
+        Fade: 'https://valorantinfo.com/images/pl/fade_valorant_icon_2351.webp',
+        Viper: 'https://static.wikia.nocookie.net/valorant/images/5/5f/Viper_icon.png',
+        Sova: 'https://static.wikia.nocookie.net/valorant/images/4/49/Sova_icon.png',
+    };
     return (
         <button className="agent-button" onClick={() => setAgent!(agentName)}>
             <img
-                className="w-16 md:w-24"
-                src={`/src/assets/agents/${agentName === 'KAY/O' ? 'KAYO' : agentName}.webp`}
+                className="w-16 md:w-24 rounded-md"
+                src={agentImgs[agentName as keyof typeof agentImgs]}
                 alt={agentName}
             />
             <h2>{agentName}</h2>
